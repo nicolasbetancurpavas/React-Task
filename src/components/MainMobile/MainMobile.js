ded27fdcb15f877f0a2bf6e57599f9eaf213d5bb
Your Name <betancur10nicolas@gmail.com>
2023-02-15 09:13:21 -0500

    mejorando mobile

--- src/components/MainMobile/MainMobile.js
+++ src/components/MainMobile/MainMobile.js
@@ -1,12 +1,19 @@
 import React, { useContext } from 'react'
 import { TodoContext } from '../TodoContext'
 import './mobile.css'
+import logo from '../../assets/img/Logo.png'
 
 export const MainMobile = () => {
     const { onClikOpen } = useContext(TodoContext)
     return (
-        <div className='main-mobile' onClick={onClikOpen}>
-            <ion-icon name="menu-outline"></ion-icon>
+        <div className='main-mobile'>
+            <ion-icon name="menu-outline"
+                onClick={onClikOpen}
+            ></ion-icon>
+            <div className='main-mobile-right'>
+                <h2 className='title-mobile'>Cat<span>ToDo</span></h2>
+                <img className='img-log-mobile' src={logo} alt='logo-mobile' />
+            </div>
         </div>
     )
 }
